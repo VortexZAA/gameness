05f0ea56fec8737e95172e1f5447595a4f7e1121
VortexZaa <abidinayhan94@gmail.com>
2024-04-19 14:17:47 +0300

    Add favicon and update title in _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head >
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <title>GAMENESS AIRDROP</title>
       <body className="bg-dark min-h-screen overflow-x-hidden font-red_hot_display">
         <Main />
         <NextScript />
